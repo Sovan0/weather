33a17a77745671fafdfc1af0ea2d355a3f995571
Adrian Carp <carpadrian99@gmail.com>
2022-11-20 04:35:40 +0200

    add repo

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e0be0dfd45msh02a9a55441532e4p1fad30jsn75908c98e25e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4683020919e99a098ec822525b5be70d";
